bd8a2d67144051e5e7478359f35882c367df5007
denisse cumbal <denissecumbal2015@hotmail.com>
2020-08-09 02:03:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include <fstream>
+
+void arch_prueba ();
+void arch_lectura ();
+using namespace std;
+
+
+int main()
+{
+   
+arch_prueba ();//
+arch_lectura ();
+
+    return 0;
+}void arch_prueba(){
+
+  
+int num;
+   ofstream archivoprueba;
+  
+
+    archivoprueba.open("piramide.txt",ios::out);
+ 
+   do
+    {
+     int num;
+  cout << "digite el tamanio de la piramide : ";
+  cin >> num;
+  int lado = num;
+  for (int i = 0; i < num; i++){
+    if (i%2 ==0){
+      for (int i = 0; i < lado; i++){
+      archivoprueba<< "* ";
+     }
+     archivoprueba <<endl;
+    }
+    else 
+    {
+      for (int i = 0; i < lado; i++){
+      archivoprueba << "- ";
+      }
+      archivoprueba <<endl;
+    }
+    lado--;
+  }
+    }
+   while(num!=0);
+  
+   
+   
+   archivoprueba.close();
+
+  
+
+       }
+    void arch_lectura (){
+   string texto;
+   int num; 
+   ifstream archivoprueba;
+   archivoprueba.open("piramide.txt",ios::in);
+   
+  while(!archivoprueba.eof())
+
+    {
+     getline(archivoprueba,texto);
+         
+        cout<<texto<<endl;
+    }
+   archivoprueba.close(); 
+}
+
